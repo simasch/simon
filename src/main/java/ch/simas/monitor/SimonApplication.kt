717db85d3f619781d67fe612dd8863e80544dcc1
Simon Martinelli <sm@simas.ch>
2016-10-18 08:18:11 +0200

    code clean up

--- src/main/java/ch/simas/monitor/SimonApplication.kt
+++ src/main/java/ch/simas/monitor/SimonApplication.kt
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class SimonApplication {
-}
+open class SimonApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(SimonApplication::class.java, *args)
